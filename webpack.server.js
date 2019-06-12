ccd429dc7b0993c04ce236090bbce8a291fbb823
codelerium <codeliriumblog@gmail.com>
2019-06-12 15:25:52 +0200

    exclude node_module files from server build

--- webpack.server.js
+++ webpack.server.js
@@ -1,7 +1,9 @@
 const path = require('path');
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
   target: 'node',
+  externals: [nodeExternals()],
   entry: ['./server.js'],
   output: {
     path: path.resolve(__dirname, '.server'),
